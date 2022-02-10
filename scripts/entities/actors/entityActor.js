b26af26c0be87c1dc10de797b38fe4badc91c3ae
karmaroms <77210927+karmaroms@users.noreply.github.com>
2022-02-10 23:20:58 +0100

    hide Agent type

--- scripts/entities/actors/entityActor.js
+++ scripts/entities/actors/entityActor.js
@@ -14,6 +14,54 @@ import { SR5_SocketHandler } from "../../socket.js";
  */
 
 export class SR5Actor extends Actor {
+
+  /** Overide Actor's create Dialog to hide certain type and sort them alphabetically*/
+  static async createDialog(data={}, {parent=null, pack=null, ...options}={}) {
+
+    // Collect data
+    const documentName = this.metadata.name;
+    const hiddenTypes = ["actorAgent"];
+    const originalTypes = game.system.documentTypes[documentName];
+    const types = originalTypes.filter(
+      (actorType) => !hiddenTypes.includes(actorType)
+    );
+    const folders = parent ? [] : game.folders.filter(f => (f.data.type === documentName) && f.displayed);
+    const label = game.i18n.localize(this.metadata.label);
+    const title = game.i18n.format("DOCUMENT.Create", {type: label});
+
+    // Render the document creation form
+    const html = await renderTemplate(`templates/sidebar/document-create.html`, {
+      name: data.name || game.i18n.format("DOCUMENT.New", {type: label}),
+      folder: data.folder,
+      folders: folders,
+      hasFolders: folders.length >= 1,
+      type: data.type || types[0],
+      types: types.reduce((obj, t) => {
+        const label = CONFIG[documentName]?.typeLabels?.[t] ?? t;
+        obj[t] = game.i18n.has(label) ? game.i18n.localize(label) : t;
+        return SR5_EntityHelpers.sortObjectValue(obj);
+      }, {}),
+      hasTypes: types.length > 1
+    });
+
+    // Render the confirmation dialog window
+    return Dialog.prompt({
+      title: title,
+      content: html,
+      label: title,
+      callback: html => {
+        const form = html[0].querySelector("form");
+        const fd = new FormDataExtended(form);
+        foundry.utils.mergeObject(data, fd.toObject(), {inplace: true});
+        if ( !data.folder ) delete data["folder"];
+        if ( types.length === 1 ) data.type = types[0];
+        return this.create(data, {parent, pack, renderSheet: true});
+      },
+      rejectClose: false,
+      options: options
+    });
+  }
+
   static async create(data, options) {
     if (!data.img) {
       data.img = `systems/sr5/img/actors/${data.type}.svg`;
