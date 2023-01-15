9dc1fafa00353dd0fe6c7b3de1fe46070ecaaea3
karmaroms <77210927+karmaroms@users.noreply.github.com>
2023-01-15 19:09:47 +0100

    Small fix for compendium (#367)

--- scripts/entities/items/utilityItem.js
+++ scripts/entities/items/utilityItem.js
@@ -307,6 +307,7 @@ export class SR5_UtilityItem extends Actor {
 
 	static _handleItemAvailability(item) {
 		let multiplier;
+		//console.log(item);
 		switch (item.availability.multiplier) {
 			case "rating":
 				multiplier = item.itemRating;
@@ -2096,7 +2097,7 @@ export class SR5_UtilityItem extends Actor {
 	}
 
 	static _handleWeaponMounted(item) {
-		let itemData = item.data, wm = itemData.weaponMount;
+		let itemData = item.system, wm = itemData.weaponMount;
 		switch (wm.size) {
 			case "light":
 				itemData.slots.base = 1;
@@ -2260,7 +2261,6 @@ export class SR5_UtilityItem extends Actor {
 			case "capacity":
 				multiplier = itemData.capacity.value;
 				break;
-			default:
 		}
 		if (itemData.threshold.multiplier) {
 			SR5_EntityHelpers.updateModifier(itemData.threshold, game.i18n.localize(SR5.valueMultipliers[itemData.threshold.multiplier]), "multiplier", multiplier, true, false);
