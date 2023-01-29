7296ff0991007dbb25d1f2fa56542f3e6f91d248
karmaroms <77210927+karmaroms@users.noreply.github.com>
2023-01-29 12:04:31 +0100

    Fix fly movement (#377)

--- scripts/entities/actors/utilityActor.js
+++ scripts/entities/actors/utilityActor.js
@@ -1336,7 +1336,7 @@ export class SR5_CharacterUtility extends Actor {
 					if (actor.type == "actorSpirit") {
 						movements[key].extraMovement.base = 5;
 					}
-					if (skills.flight.rating.value > 0) {
+					if (skills.flight?.rating?.value > 0) {
 						SR5_EntityHelpers.updateModifier(movements[key].movement, game.i18n.localize('SR5.Agility'), "linkedAttribute", attributes.agility.augmented.value * movements.run.multiplier.value);
 						SR5_EntityHelpers.updateModifier(movements[key].test, game.i18n.localize('SR5.Strength'), "linkedAttribute", attributes.strength.augmented.value);
 						SR5_EntityHelpers.updateModifier(movements[key].test, game.i18n.localize('SR5.SkillFly'), "skillRating", skills.flight.rating.value);
